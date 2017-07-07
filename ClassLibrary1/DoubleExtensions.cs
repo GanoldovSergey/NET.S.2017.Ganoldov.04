4911c1bce9e4740dd3dda44146e638bf230d9258
sergey.ganoldov <sergey.ganoldov@gmail.com>
2017-07-07 14:25:50 +0300

    1

---
+++ ClassLibrary1/DoubleExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class MathExtensions
+    {
+        /// <summary>
+        /// Newton's algorithm for getting n-th root of number.
+        /// </summary>
+        /// <param name="a">A number to get root</param>
+        /// <param name="n">Root power</param>
+        /// <param name="eps">Accuracy</param>
+        /// <returns> N-th root of 'a'</returns>
+        public static double Root(double a, int n, double eps = 0.0001)
+        {
+            if (a <= 0) throw new ArgumentException($"{nameof(a)} must be positive");
+            if (n < 1) throw new ArgumentException($"{nameof(n)} must be greater than 0");
+            if (eps < 0) throw new ArgumentException($"{nameof(eps)} must be positive");
+
+            double x0 = a / n;
+            double x1 = (1.0 / n) * ((n - 1) * x0 + a / Math.Pow(x0, n - 1));
+
+            while (Math.Abs(x0 - x1)> eps)
+            {
+                x0 = x1;
+                x1 = (1.0 / n) * ((n - 1) * x0 + a / Math.Pow(x0, n - 1));
+            }
+
+            return x1;
+        }
+    }
+}
---
+++ Logic.NUnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Logic.NUnitTests
+{
+    public class UnitTest1
+    {
+        [TestCase(4, 2, 0.00001)]
+        [TestCase(54, 3, 0.00001)]
+        [TestCase(543.887, 6, 0.00001)]
+        [TestCase(0.001, 3, 0.00001)]
+        public void PositiveTest(double a, int n, double eps)
+        {
+            Assert.IsTrue(Math.Abs(MathExtensions.Root(a, n, eps) - Math.Pow(a, 1.0 / n)) < eps);
+        }
+
+        [TestCase(5, 2, -0.5)]
+        [TestCase(3, 0, 0.1)]
+        [TestCase(-8, 3, 0.1)]
+        public void GetRoot_ThrowsArgumentException(double a, int n, double eps)
+        {
+            Assert.Throws<ArgumentException>(
+                () => MathExtensions.Root(a, n, eps));
+        }
+    }
+}
